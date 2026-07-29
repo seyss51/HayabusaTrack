729222a3521441b699fd33976fc6f3178ea9a9d2
seyss51 <seyss51@gmail.com>
2026-07-29 12:05:15 +0200

    Update build.gradle.kts

--- wear/build.gradle.kts
+++ wear/build.gradle.kts
@@ -36,6 +36,8 @@ dependencies {
     implementation("androidx.compose.material:material:1.6.8")
     implementation("com.google.android.gms:play-services-wearable:18.2.0")
     implementation("com.google.android.gms:play-services-location:21.3.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.8.1")
     implementation("androidx.room:room-runtime:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
     ksp("androidx.room:room-compiler:2.6.1")
