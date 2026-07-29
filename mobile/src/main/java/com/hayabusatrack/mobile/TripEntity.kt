e580dfa6bf00c58575cdd1df06f1116ceff93fd5
seyss51 <seyss51@gmail.com>
2026-07-29 09:17:51 +0200

    Create mobile/src/main/java/com/hayabusatrack/mobile/TripEntity.kt

---
+++ mobile/src/main/java/com/hayabusatrack/mobile/TripEntity.kt
@@ -0,0 +1,16 @@
+package com.hayabusatrack.mobile
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "trips")
+data class TripEntity(
+    @PrimaryKey val id: String,
+    val startTime: Long,
+    val endTime: Long,
+    val maxSpeedKmh: Float,
+    val maxAccelMs2: Float,
+    val maxDecelMs2: Float,
+    val maxLeanAngleDeg: Float,
+    val distanceMeters: Float
+)
